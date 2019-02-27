55cdaef662232e02b50a531e79ce2f6427892e20
Valiaiev Kyrylo <kirill.sing2000@gmail.com>
2019-02-27 17:59:20 +0100

    first programm

--- JavaProject/src/com/ap/valiaiev/Main.java
+++ JavaProject/src/com/ap/valiaiev/Main.java
@@ -1,10 +1,26 @@
 package com.ap.valiaiev;
 
+import java.io.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+		throws IOException
+	{
 
+		double a, b, pole;
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Program oblicza pole prostok¹ta.");
+		System.out.println("Podaj bok a.");
+		a = Double.parseDouble(br.readLine());
+		System.out.println("Podaj bok a. ");
+		b = Double.parseDouble(br.readLine());
+		pole = a * b;
+
+		System.out.print("Pole prostok¹t¹ o boku a = " + a + " i boku b = " + b);
+		System.out.println(" wynosi " + pole + ".");
 	}
 
 }
