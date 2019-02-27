861001e3dc05e012a4825993573871e87cfee7ac
Valiaiev Kyrylo <kirill.sing2000@gmail.com>
2019-02-27 19:17:49 +0100

    second programm

---
+++ JavaProject/src/com/ap/valiaiev/Zegar.java
@@ -0,0 +1,25 @@
+package com.ap.valiaiev;
+
+import java.util.*;
+
+public class Zegar {
+	
+		public ArrayList<Integer> oblicz(int a, int b, int c, int d)
+		{
+			ArrayList<Integer> numberList = new ArrayList<>();
+			numberList.add(a);
+			numberList.add(b);
+			numberList.add(c);
+			numberList.add(d);
+			
+			Collections.sort(numberList);	
+//			Collections.reverse(numberList);
+			
+			if()
+			{
+				
+			}
+			return numberList;
+		}
+}
+
