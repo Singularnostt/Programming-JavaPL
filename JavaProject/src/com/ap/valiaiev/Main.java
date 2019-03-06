8d0d19c0e4f645b021708930e661022174f0d1b6
Valiaiev Kyrylo <kirill.sing2000@gmail.com>
2019-03-06 18:17:09 +0100

    Half of work

--- JavaProject/src/com/ap/valiaiev/Main.java
+++ JavaProject/src/com/ap/valiaiev/Main.java
@@ -7,10 +7,16 @@ public class Main {
 	public static void main(String[] args)
 	throws IOException
 	{
-		Double a,b,c,d;
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
+		
         Zegar zegar = new Zegar();
-
+        
+        zegar.oblicz(a,b,c,d);
 
         System.out.println(zegar);
 	}
--- JavaProject/src/com/ap/valiaiev/Zegar.java
+++ JavaProject/src/com/ap/valiaiev/Zegar.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Zegar {
 	
 		public ArrayList<Integer> oblicz(int a, int b, int c, int d)
+		
 		{
+			int n=4;
 			ArrayList<Integer> numberList = new ArrayList<>();
 			numberList.add(a);
 			numberList.add(b);
@@ -15,11 +17,40 @@ public class Zegar {
 			Collections.sort(numberList);	
 //			Collections.reverse(numberList);
 			
-			if()
+			if(numberList.get(0)<=2)
 			{
-				
+				if(numberList.get(1)<=4)
+				{
+					if(numberList.get(2)<=5)
+					{
+						if(numberList.get(3)<=9)
+						{
+							for (int i = 0; i < numberList.size(); i++) {
+							     
+							   
+							}
+				        }  
+							System.out.println("Zegar" + numberList);
+						}
+						else 
+						{
+							System.out.println("To nie jest zegar");
+						}
+					}
+					else 
+					{
+						System.out.println("To nie jest zegar");
+					}
+				}
+				else 
+				{
+					System.out.println("To nie jest zegar");
+				}
+			}
+			else
+			{
+				System.out.println("To nie jest zegar");
 			}
 			return numberList;
 		}
 }
-
